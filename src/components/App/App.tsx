64235820929680855600ccc9d8d90d09012c019c
Damir Gimatdinov <damirgim@yandex.com>
2022-09-21 14:26:08 +0400

    Correcting some stuff

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './App.scss';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
 }
 
 export default App;
