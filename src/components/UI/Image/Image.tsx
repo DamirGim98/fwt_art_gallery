d0013ec2d8e3014cb15c5e45497b6ca096ba433d
Damir Gimatdinov <damirgim@yandex.com>
2022-10-08 11:11:56 +0400

    Adds working LazyLoading to Image component

--- src/components/UI/Image/Image.tsx
+++ src/components/UI/Image/Image.tsx
@@ -8,9 +8,10 @@ import styles from './Image.module.scss';
 export interface IImageProps {
   image: IImage;
   className?: string;
+  onLoad?(): void;
 }
 
-const Image: FC<IImageProps> = ({ image, className }) => {
+const Image: FC<IImageProps> = ({ image, className, onLoad }) => {
   const cx = cn.bind(styles);
   const [isLoaded, setIsLoaded] = useState(false);
   const imageRef = useRef<HTMLImageElement | null>(null);
@@ -24,24 +25,40 @@ const Image: FC<IImageProps> = ({ image, className }) => {
     if (imageRef.current) {
       imageRef.current.onload = () => {
         setIsLoaded(true);
+        if (onLoad) onLoad();
       };
     }
-  }, [isVisible, isLoaded]);
+  }, [isVisible, isLoaded, onLoad]);
 
   return (
-    <picture ref={containerRef} className={cx('container')} key={image._id}>
-      <source type="image/webp" srcSet={`${BASE_URL}${image.webp}`} media={'(max-width: 576px)'} />
-      <source
-        type="image/webp"
-        srcSet={`${BASE_URL}${image.webp2x}`}
-        media={'(min-width: 576px)'}
-      />
-      <source type="image/jpg" srcSet={`${BASE_URL}${image.src}`} media={'(max-width: 576px)'} />
-      <source type="image/jpg" srcSet={`${BASE_URL}${image.src2x}`} media={'(min-width: 576px)'} />
-      {(isVisible || isLoaded) && (
-        <img ref={imageRef} className={className} src={`${BASE_URL}${image.src}`} alt={'picture'} />
-      )}
-    </picture>
+    <div ref={containerRef} className={cx('container', className)}>
+      <picture key={image._id}>
+        <source
+          type="image/webp"
+          srcSet={`${BASE_URL}${image.webp}`}
+          media={'(max-width: 576px)'}
+        />
+        <source
+          type="image/webp"
+          srcSet={`${BASE_URL}${image.webp2x}`}
+          media={'(min-width: 576px)'}
+        />
+        <source type="image/jpg" srcSet={`${BASE_URL}${image.src}`} media={'(max-width: 576px)'} />
+        <source
+          type="image/jpg"
+          srcSet={`${BASE_URL}${image.src2x}`}
+          media={'(min-width: 576px)'}
+        />
+        {(isVisible || isLoaded) && (
+          <img
+            ref={imageRef}
+            className={cx('image')}
+            src={`${BASE_URL}${image.src}`}
+            alt={'picture'}
+          />
+        )}
+      </picture>
+    </div>
   );
 };
 
