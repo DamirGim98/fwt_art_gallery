db24ced27d280017f77417d0b8aeb59569edd735
Damir Gimatdinov <damirgim@yandex.com>
2022-10-08 10:10:19 +0400

    Adds  LazyLoading for image

--- src/components/UI/Image/Image.tsx
+++ src/components/UI/Image/Image.tsx
@@ -1,6 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState, useRef } from 'react';
+import cn from 'classnames/bind';
 import { IImage } from '../../../types/types';
 import { BASE_URL } from '../../../Api/API';
+import useLazyLoading from '../../../hooks/useLazyLoading';
+import styles from './Image.module.scss';
 
 export interface IImageProps {
   image: IImage;
@@ -8,8 +11,25 @@ export interface IImageProps {
 }
 
 const Image: FC<IImageProps> = ({ image, className }) => {
+  const cx = cn.bind(styles);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const imageRef = useRef<HTMLImageElement | null>(null);
+  const containerRef = React.useRef<HTMLDivElement | null>(null);
+  const isVisible = useLazyLoading(containerRef);
+
+  useEffect(() => {
+    if (!isVisible || isLoaded) {
+      return;
+    }
+    if (imageRef.current) {
+      imageRef.current.onload = () => {
+        setIsLoaded(true);
+      };
+    }
+  }, [isVisible, isLoaded]);
+
   return (
-    <picture key={image._id}>
+    <picture ref={containerRef} className={cx('container')} key={image._id}>
       <source type="image/webp" srcSet={`${BASE_URL}${image.webp}`} media={'(max-width: 576px)'} />
       <source
         type="image/webp"
@@ -18,7 +38,9 @@ const Image: FC<IImageProps> = ({ image, className }) => {
       />
       <source type="image/jpg" srcSet={`${BASE_URL}${image.src}`} media={'(max-width: 576px)'} />
       <source type="image/jpg" srcSet={`${BASE_URL}${image.src2x}`} media={'(min-width: 576px)'} />
-      <img className={className} src={`${BASE_URL}${image.src}`} alt={'picture'} />
+      {(isVisible || isLoaded) && (
+        <img ref={imageRef} className={className} src={`${BASE_URL}${image.src}`} alt={'picture'} />
+      )}
     </picture>
   );
 };
---
+++ src/hooks/useLazyLoading.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect, MutableRefObject } from 'react';
+
+function useLazyLoading(ref: MutableRefObject<HTMLElement | null>, rootMargin = '0px') {
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIntersecting(entry.isIntersecting);
+      },
+      {
+        rootMargin,
+      },
+    );
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  return isIntersecting;
+}
+
+export default useLazyLoading;
